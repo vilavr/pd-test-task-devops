9187b3de7241784cb66e12b608bbb834f553bf30
Vira Lavrova <vira.lavrova@transferwise.com>
2024-07-30 14:10:37 +0300

    clean up ci and reconfigure jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageReporters: ['json-summary', 'lcov', 'text', 'clover'],
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'lcov', 'text', 'clover'],
+  reporters: [
+    'default',
+    [
+      'jest-json-reporter',
+      {
+        outputFile: 'test-results/test-results.json',
+      },
+    ],
+  ],
+};
