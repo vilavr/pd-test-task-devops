4144bc57b726e1ab1773c33f2cc489abc0eb3ff9
Vira Lavrova <vira.lavrova@transferwise.com>
2024-07-30 11:49:00 +0300

    jest config to support coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts']
+    testMatch: ['**/tests/**/*.test.ts'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
   };
   
