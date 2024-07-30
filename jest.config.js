8fbb26c3d2280b8cc7a7ca0c75b79378479a647c
Vira Lavrova <vira.lavrova@transferwise.com>
2024-07-30 14:30:13 +0300

    revert to ctrf

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['json-summary', 'lcov', 'text', 'clover'],
-  reporters: [
-    'default',
-    [
-      'jest-json-reporter',
-      {
-        outputFile: 'test-results/test-results.json',
-      },
-    ],
-  ],
-};
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.ts'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['json-summary', 'lcov', 'text', 'clover'],
+  };
+  
