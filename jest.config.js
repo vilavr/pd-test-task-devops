f1d68f941ce6fe8a96f6b85e0d7a6233fa0d0fba
Vira Lavrova <vira.lavrova@transferwise.com>
2024-07-31 10:30:31 +0300

    delete redundant config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageReporters: ['json-summary', 'lcov', 'text', 'clover'],
-  };
-  
