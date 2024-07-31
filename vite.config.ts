8ebbec8ddf264cd72227b92dd1be45ac3c6756b9
Vira Lavrova <vira.lavrova@transferwise.com>
2024-07-31 10:55:36 +0300

    use ctrf to visualise test results (#4)
    * use ctrf to visualise test results
    
    * nyc coverage report in comment

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { configDefaults } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
+  },
+});
